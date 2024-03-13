84b0b5058aad6925aeb8a936303c5b1b83fd12cb
Rolan Sh <rolansh@gmail.com>
2024-03-13 11:17:01 +0200

    Upgrading msgraph api

--- using-graph-sdk/src/main/java/org/plutext/msgraph/convert/graphsdk/Limited4MB.java
+++ using-graph-sdk/src/main/java/org/plutext/msgraph/convert/graphsdk/Limited4MB.java
@@ -18,43 +18,30 @@
 
 package org.plutext.msgraph.convert.graphsdk;
 
-import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-import java.util.stream.Stream;
 
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.microsoft.graph.models.DriveItem;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.plutext.msgraph.convert.DocxToPdfConverter;
-import org.plutext.msgraph.convert.OpenXmlToPDF;
 import org.plutext.msgraph.convert.AbstractOpenXmlToPDF;
 import org.plutext.msgraph.convert.AuthConfig;
 import org.plutext.msgraph.convert.ConversionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.microsoft.graph.auth.confidentialClient.ClientCredentialProvider;
-import com.microsoft.graph.auth.enums.NationalCloud;
-import com.microsoft.graph.concurrency.ICallback;
-import com.microsoft.graph.core.ClientException;
-import com.microsoft.graph.models.extensions.DriveItem;
-import com.microsoft.graph.models.extensions.IGraphServiceClient;
-import com.microsoft.graph.requests.extensions.GraphServiceClient;
-
 /**
  * Demonstrate using the Graph SDK high level API for PDF Conversion.
  * 
  * This doesn't support converting files bigger than 4MB, so you should use PdfConverterLarge instead.  
  * 
- * Note, neither update your TOC. Vote for this enhancement at 
- * https://microsoftgraph.uservoice.com/forums/920506-microsoft-graph-feature-requests/suggestions/41235295-docx-to-pdf-file-conversion-update-table-of-conte 
- * 
+ *
  * @author jharrop
  *
  */
@@ -68,42 +55,49 @@ public class Limited4MB extends AbstractOpenXmlToPDF {
 			
 	@Override
 	public byte[] convert(byte[] bytes, String ext) throws ConversionException {
-		
-		
-    	List<String> scopes = new ArrayList<String>();
-    	scopes.add("https://graph.microsoft.com/.default");
-		ClientCredentialProvider authProvider = 
-				new ClientCredentialProvider(authConfig.apiKey(), scopes, authConfig.apiSecret(), 
-						authConfig.tenant(), NationalCloud.Global);	
-		
-//		Using msgraph-sdk-java
-		IGraphServiceClient graphClient = GraphServiceClient
-						.builder()
-						.authenticationProvider(authProvider)
-						.buildClient();
+		// The client credentials flow requires that you request the
+		// /.default scope, and pre-configure your permissions on the
+		// app registration in Azure. An administrator must grant consent
+		// to those permissions beforehand.
+		final String[] scopes = new String[] { "https://graph.microsoft.com/.default" };
 
-		// path = "https://graph.microsoft.com/v1.0/sites/" + siteId + "/drive/items/";
-		
-		
-        String tmpFileName = UUID.randomUUID().toString() + ".docx"; // an extension is required  
-        
-//        String requestUrl = path +"root:/" + tmpFileName + ":/content";		
-		String convertPathPrefix = "/sites/" + authConfig.site() + "/drive/items/";
-		String item =  "root:/" + tmpFileName +":";	
-		// or better, use buildRequest( requestOptions )
+		// Authentication provider - Client credentials provider - Using a client secret
+		// https://learn.microsoft.com/en-us/graph/sdks/choose-authentication-providers?tabs=java
+		final ClientSecretCredential credential = new ClientSecretCredentialBuilder()
+				.clientId(authConfig.apiKey())
+				.tenantId(authConfig.tenant())
+				.clientSecret(authConfig.apiSecret())
+				.build();
 
-        // Note the obscure code
-		MyCallback myCallback = new MyCallback(graphClient, convertPathPrefix, authConfig.site(), item);
-		graphClient.sites(authConfig.site()).drive().items(item).content().buildRequest()
-		.put(bytes, myCallback );
+		final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
 		
-		// wait
+        String tmpFileName = UUID.randomUUID().toString() + ".docx"; // an extension is required
+		String itemPath =  "root:/" + tmpFileName +":";
+
+		// Determined the driveId
+		String driveId = graphClient.sites().bySiteId(authConfig.site()).drive().get().getId();
+
+		// Upload
+		DriveItem driveItem = graphClient.drives()
+				.byDriveId(driveId)
+				.items()
+				.byDriveItemId(itemPath)
+				.content()
+				.put(new ByteArrayInputStream(bytes));
+
+		// Download as pdf
+		InputStream inputStream = graphClient.drives().byDriveId(driveId).items().byDriveItemId(itemPath).content().get(requestConfiguration -> {
+			requestConfiguration.queryParameters.format = "pdf";
+		});
+
+
 		try {
-			myCallback.ft.get();
+			// Move to trash
+			graphClient.drives().byDriveId(driveId).items().byDriveItemId(itemPath).delete();
+			return IOUtils.toByteArray(inputStream);
 		} catch (Exception e) {
 			throw new ConversionException(e.getMessage(), e);
 		}
-		return myCallback.pdf;
 		
 	}
 
@@ -121,73 +115,6 @@ public class Limited4MB extends AbstractOpenXmlToPDF {
 		return convert( IOUtils.toByteArray(docx), ext );
 	}	
 	
-	static class MyCallback implements ICallback<DriveItem> {
-
-		MyCallback(IGraphServiceClient graphClient, String convertPathPrefix, String site, String item) {
-			this.graphClient = graphClient;
-			this.convertPathPrefix = convertPathPrefix;
-			this.site = site;
-			this.item = item;
-		}
-		
-		String convertPathPrefix;
-		String site;		
-		String item;
-		IGraphServiceClient graphClient;
-		
-		byte[] pdf;
-
-		final FutureTask<Object> ft = new FutureTask<Object>(() -> {}, new Object());			
-		
-		@Override
-		public void success(DriveItem result) {
-						
-//			Option format = new PdfOption("format", "pdf");
-//			List<Option> requestOptions = new ArrayList<Option>();
-//			requestOptions.add(format);
-			
-			try (
-					BufferedInputStream inputStream = (BufferedInputStream)graphClient.customRequest(convertPathPrefix + item+ "/content?format=pdf", Stream.class)
-					.buildRequest()
-					.get();
-					
-					// Both the following ignores format option!
-//					BufferedInputStream inputStream = (BufferedInputStream)graphClient.customRequest(convertPathPrefix + item+ "/content", Stream.class)
-//					.buildRequest(requestOptions)
-//					.get();
-					// customRequest request works, but the following is neater
-//					BufferedInputStream inputStream = (BufferedInputStream)graphClient.sites(siteId).drive().items(item).content()
-//						.buildRequest( requestOptions ).get();
-			
-	        ) {
-				pdf = IOUtils.toByteArray(inputStream);
-
-	        } catch (ClientException e) {
-	        	log.error(e.getMessage(), e);
-	        	throw new RuntimeException(e.getMessage(), e);
-			} catch (IOException e) {
-	        	log.error(e.getMessage(), e);
-	        	throw new RuntimeException(e.getMessage(), e);
-			}			
-						
-			// Move to recycle bin
-			graphClient.sites(site).drive().items(item)
-					.buildRequest().delete();
-			
-			ft.run(); // so we can know this callback has been finished
-			
-		}
-
-		@Override
-		public void failure(ClientException ex) {
-			log.error("Conversion failed", ex);
-			
-		}
-
-		
-	}
-
-
 	
 
 }
--- using-graph-sdk/src/main/java/org/plutext/msgraph/convert/graphsdk/PdfConverterLarge.java
+++ using-graph-sdk/src/main/java/org/plutext/msgraph/convert/graphsdk/PdfConverterLarge.java
@@ -23,9 +23,11 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 import java.util.stream.Stream;
@@ -39,16 +41,20 @@ import org.plutext.msgraph.convert.DocxToPdfConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.microsoft.graph.auth.confidentialClient.ClientCredentialProvider;
-import com.microsoft.graph.auth.enums.NationalCloud;
-import com.microsoft.graph.concurrency.ChunkedUploadProvider;
-import com.microsoft.graph.concurrency.IProgressCallback;
-import com.microsoft.graph.core.ClientException;
-import com.microsoft.graph.models.extensions.DriveItem;
-import com.microsoft.graph.models.extensions.DriveItemUploadableProperties;
-import com.microsoft.graph.models.extensions.IGraphServiceClient;
-import com.microsoft.graph.models.extensions.UploadSession;
-import com.microsoft.graph.requests.extensions.GraphServiceClient;
+import com.azure.identity.ClientCertificateCredential;
+import com.azure.identity.ClientCertificateCredentialBuilder;
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.microsoft.graph.core.exceptions.ClientException;
+import com.microsoft.graph.core.models.IProgressCallback;
+import com.microsoft.graph.core.models.UploadResult;
+import com.microsoft.graph.core.tasks.LargeFileUploadTask;
+import com.microsoft.graph.drives.item.items.item.createuploadsession.CreateUploadSessionPostRequestBody;
+import com.microsoft.graph.models.Drive;
+import com.microsoft.graph.models.DriveItem;
+import com.microsoft.graph.models.DriveItemUploadableProperties;
+import com.microsoft.graph.models.UploadSession;
+import com.microsoft.graph.serviceclient.GraphServiceClient;
 
 /**
  * Demonstrate using the Graph SDK high level API for PDF Conversion.
@@ -72,62 +78,103 @@ public class PdfConverterLarge  extends AbstractOpenXmlToPDF {
 			
 
 	public byte[] convert(InputStream fileStream, long streamSize, String ext) throws ConversionException, IOException {
-		
-    	List<String> scopes = new ArrayList<String>();
-    	scopes.add("https://graph.microsoft.com/.default");
-		ClientCredentialProvider authProvider = 
-				new ClientCredentialProvider(authConfig.apiKey(), scopes, authConfig.apiSecret(), 
-						authConfig.tenant(), NationalCloud.Global);	
-		
-//		Using msgraph-sdk-java
-		IGraphServiceClient graphClient = GraphServiceClient
-						.builder()
-						.authenticationProvider(authProvider)
-						.buildClient();
+		// TODO: Pending fix, com.microsoft.kiota.ApiException: generalException
+		// https://github.com/microsoftgraph/msgraph-sdk-java/issues/1806
 
-		// path = "https://graph.microsoft.com/v1.0/sites/" + siteId + "/drive/items/";
+		// The client credentials flow requires that you request the
+		// /.default scope, and pre-configure your permissions on the
+		// app registration in Azure. An administrator must grant consent
+		// to those permissions beforehand.
+		final String[] scopes = new String[] { "https://graph.microsoft.com/.default" };
+
+		// Authentication provider - Client credentials provider - Using a client secret
+		// https://learn.microsoft.com/en-us/graph/sdks/choose-authentication-providers?tabs=java
+		final ClientSecretCredential credential = new ClientSecretCredentialBuilder()
+				.clientId(authConfig.apiKey())
+				.tenantId(authConfig.tenant())
+				.clientSecret(authConfig.apiSecret())
+				.build();
+
+		final GraphServiceClient graphClient = new GraphServiceClient(credential, scopes);
 		
 		
-        String tmpFileName = UUID.randomUUID().toString() + ext;
-        
-//        String requestUrl = path +"root:/" + tmpFileName + ":/content";		
-		String convertPathPrefix = "/sites/" + authConfig.site() + "/drive/items/";
-		String item =  "root:/" + tmpFileName +":";	
+		String tmpFileName = UUID.randomUUID().toString() + ext;
+		String itemPath =  "root:/" + tmpFileName +":";
 
-		
-		// support more than 4MB, using large file uploader; see https://docs.microsoft.com/en-us/graph/sdks/large-file-upload?tabs=java
+		// Set body of the upload session request
+		CreateUploadSessionPostRequestBody uploadSessionRequest = new CreateUploadSessionPostRequestBody();
+		DriveItemUploadableProperties properties = new DriveItemUploadableProperties();
+		properties.getAdditionalData().put("@microsoft.graph.conflictBehavior", "replace");
+		uploadSessionRequest.setItem(properties);
 
-		// Create an upload session
-		UploadSession uploadSession = graphClient
-				.sites(authConfig.site()).drive().items(item)
-		    .createUploadSession(new DriveItemUploadableProperties())
-		    .buildRequest()
-		    .post();
+		// Support more than 4MB, using large file uploader; see https://docs.microsoft.com/en-us/graph/sdks/large-file-upload?tabs=java
+		String driveId = graphClient.sites().bySiteId(authConfig.site()).drive().get().getId();
+		UploadSession uploadSession = graphClient.drives()
+				.byDriveId(driveId)
+				.items()
+				.byDriveItemId(itemPath)
+				.createUploadSession()
+				.post(uploadSessionRequest);
 
-		ChunkedUploadProvider<DriveItem> chunkedUploadProvider =
-		    new ChunkedUploadProvider<DriveItem>
-		        (uploadSession, graphClient, fileStream, streamSize, DriveItem.class);
-
-		// Config parameter is an array of integers
-		// customConfig[0] indicates the max slice size
-		// Max slice size must be a multiple of 320 KiB
-		int[] customConfig = { 320 * 1024 };
-
-		// Do the upload
-		MyCallback myCallback = new  MyCallback(graphClient, convertPathPrefix, authConfig.site(), item);
-		
-		chunkedUploadProvider.upload(
-				myCallback, 
-				customConfig);
-		
-		// wait
+		// Create the upload task
+		int maxSliceSize = 10 * 320 * 1024;
+		LargeFileUploadTask<DriveItem> largeFileUploadTask = null;
 		try {
-			myCallback.ft.get();
-		} catch (Exception e) {
+			largeFileUploadTask = new LargeFileUploadTask<>(
+					graphClient.getRequestAdapter(),
+					uploadSession,
+					fileStream,
+					streamSize,
+					maxSliceSize,
+					DriveItem::createFromDiscriminatorValue);
+		} catch (IllegalAccessException e) {
+			throw new ConversionException(e.getMessage(), e);
+		} catch (InvocationTargetException e) {
+			throw new ConversionException(e.getMessage(), e);
+		} catch (NoSuchMethodException e) {
 			throw new ConversionException(e.getMessage(), e);
 		}
-		return myCallback.pdf;
-		
+
+		int maxAttempts = 5;
+		// Create a callback used by the upload provider
+		IProgressCallback callbackVerbose = (current, max) -> log.debug(
+				String.format("Uploaded %d bytes of %d total bytes", current, max));
+
+		// Do the upload
+		try {
+			UploadResult<DriveItem> uploadResult = largeFileUploadTask.upload(maxAttempts, callbackVerbose);
+			if (uploadResult.isUploadSuccessful()) {
+				log.debug(
+						String.format("Uploaded file with ID: %s", uploadResult.itemResponse.getId())
+				);
+
+				// Download as pdf
+				InputStream inputStream = graphClient.drives().byDriveId(driveId).items().byDriveItemId(itemPath).content().get(requestConfiguration -> {
+					requestConfiguration.queryParameters.format = "pdf";
+				});
+
+
+				try {
+					// Move to trash
+					graphClient.drives().byDriveId(driveId).items().byDriveItemId(itemPath).delete();
+					return IOUtils.toByteArray(inputStream);
+				} catch (Exception e) {
+					throw new ConversionException(e.getMessage(), e);
+				}
+
+			} else {
+				log.debug(
+					String.format("Failed uploading file.")
+				);
+			}
+		} catch (CancellationException e) {
+			log.debug("Error uploading: " + e.getMessage());
+			throw new ConversionException(e.getMessage(), e);
+		} catch (InterruptedException e) {
+			throw new ConversionException(e.getMessage(), e);
+		}
+
+		return  null;
 	}	
 	
 
@@ -156,90 +203,5 @@ public class PdfConverterLarge  extends AbstractOpenXmlToPDF {
 	public byte[] convert(InputStream docx, String ext) throws ConversionException, IOException {
 		// inefficient, but we need length
 		return convert( IOUtils.toByteArray(docx), ext );
-	}	
-	
-	
-	static class MyCallback implements IProgressCallback<DriveItem> {
-
-		MyCallback(IGraphServiceClient graphClient, String convertPathPrefix, String site, String item) {
-			this.graphClient = graphClient;
-			this.convertPathPrefix = convertPathPrefix;
-			this.site = site;
-			this.item = item;
-		}
-		
-		String convertPathPrefix;
-		String site;
-		String item;
-		IGraphServiceClient graphClient;
-		
-		byte[] pdf;
-
-		final FutureTask<Object> ft = new FutureTask<Object>(() -> {}, new Object());			
-		
-		@Override
-		public void success(DriveItem result) {
-	        log.debug(
-		            String.format("Uploaded file with ID: %s", result.id)
-		        );
-	        log.debug(""+result.size);
-			
-			//         String requestUrl = path + fileId + "/content?format=" + targetFormat;
-//			graphClient.sites(siteId).drive().items("root:/" +tmpFileName+":")..content()..buildRequest()
-
-//			Option format = new PdfOption("format", "pdf");
-//			List<Option> requestOptions = new ArrayList<Option>();
-//			requestOptions.add(format);
-			
-			try (
-					BufferedInputStream inputStream = (BufferedInputStream)graphClient.customRequest(convertPathPrefix + item+ "/content?format=pdf", Stream.class)
-					.buildRequest()
-					.get();
-					
-					// Both the following ignores format option!
-//					BufferedInputStream inputStream = (BufferedInputStream)graphClient.customRequest(convertPathPrefix + item+ "/content", Stream.class)
-//					.buildRequest(requestOptions)
-//					.get();
-					// customRequest request works, but the following is neater
-//					BufferedInputStream inputStream = (BufferedInputStream)graphClient.sites(siteId).drive().items(item).content()
-//						.buildRequest( requestOptions ).get();
-			
-	        ) {
-				pdf = IOUtils.toByteArray(inputStream);
-	        } catch (ClientException e) {
-	        	log.error(e.getMessage(), e);
-	        	throw new RuntimeException(e.getMessage(), e);
-			} catch (IOException e) {
-	        	log.error(e.getMessage(), e);
-	        	throw new RuntimeException(e.getMessage(), e);
-			}			
-						
-			// Move to recycle bin
-			graphClient.sites(site).drive().items(item)
-					.buildRequest().delete();
-			
-			ft.run(); // so we can know this callback has been finished
-			
-		}
-
-
-	    public void failure(final ClientException ex) {
-	        log.error(
-	            String.format("Error uploading file: %s", ex.getMessage())
-	        );
-	    }
-
-	    @Override
-	    // Called after each slice of the file is uploaded
-	    public void progress(final long current, final long max) {
-	        log.debug(
-	            String.format("Uploaded %d bytes of %d total bytes", current, max)
-	        );
-	    }
-
-		
 	}
-
-
-	
 }
